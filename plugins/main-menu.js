0ad42ae7f6026995edebe195e75c646680f6589a
Qadeer-bhai <ummerkulachi@gmail.com>
2025-06-07 11:12:57 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -15,7 +15,7 @@ cmd({
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        let dec = `╔══〔 *${config.BOT_NAME}* 〕═╗
+        let dec = `╔═〔 *${config.BOT_NAME}* 〕╗
 ║ Owner     : *${config.OWNER_NAME}*
 ║ Library   : *Baileys MD*
 ║ Type      : *Node.js*
