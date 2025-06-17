6659d91be3a6be713acec16a94cb1eca1bd91d77
Qadeer-bhai <ummerkulachi@gmail.com>
2025-06-17 09:24:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Qadeer-Bhai~AVghkZhA#asatTaKfGMdDgJ9AvQlfndruI0K6Zx87xiP6NhG5QhQ",
+SESSION_ID: process.env.SESSION_ID || "Qadeer-Bhai~Vd4VmSCY#6gY-xOoAMSX0oeFfp46AypbnGVolxGtn9ReSkWgxTd8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,14 +13,8 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY QADEER-XTECH 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY QADEER-XTECH 💮*",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://qu.ax/bBkkd.jpg",
@@ -39,13 +33,13 @@ DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923079749129",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Qadeer-Khan",
+OWNER_NAME: process.env.OWNER_NAME || "QADEER-XTECH",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© Powered by Qadeer Khan*",
+DESCRIPTION: process.env.DESCRIPTION || "*© Pᴏᴡᴇʀᴇᴅ Bʏ QADEER-XTECH*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/bBkkd.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *QADEER-XTECH*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Am Active *QADEER-XTECH*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -55,7 +49,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
@@ -77,11 +71,6 @@ ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
-// make it true for auto recoding
-ANTICALL: process.env.ANTICALL || "false",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
-OPENAI_API_KEY: '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'
-//Qadeer
-};    
-    
+};
